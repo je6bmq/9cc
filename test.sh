bcf8ca9eb4b42c80c9f8149462cb5af2c4e25185
matchaBread <je6bmq@gmail.com>
2019-07-30 22:35:38 +0900

    impl compiler which compiles a integer

---
+++ test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+try() {
+    expected="$1"
+    input="$1"
+
+    tmp_s=$(mktemp ./XXXXXX.s)
+    tmp_o=$(mktemp ./XXXXXX)
+
+    ./9cc "$input" > $tmp_s
+    gcc -o $tmp_o $tmp_s
+    $tmp_o
+
+    actual="$?"
+    rm -f $tmp_o $tmp_s
+    
+    if [ "$actual" = "$expected" ]; then
+        echo "$input => $actual"
+    else
+        echo "$input => $expected expected, but got $actual"
+        exit 1
+    fi
+}
+
+try 0 0 
+try 42 42
+
+echo "OK"
