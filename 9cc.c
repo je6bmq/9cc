aa22f35e5be95320d5c444d5604d57267343a45a
matchaBread <je6bmq@gmail.com>
2019-07-30 23:27:23 +0900

    impl simple add/sub

--- 9cc.c
+++ 9cc.c
@@ -9,9 +9,28 @@ int main(int argc, char **argv)
         return 1;
     }
 
+    char* p = argv[1];
+
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    printf("    mov rax, %d\n", atoi(argv[1]));
+    printf("    mov rax, %ld\n", strtol(p, &p, 10));
+
+    while(*p) {
+        switch(*p) {
+            case '+':
+                p++;
+                printf("    add rax, %ld\n",strtol(p, &p, 10));
+                break;
+            case '-':
+                p++;
+                printf("    sub rax, %ld\n",strtol(p, &p, 10));
+                break;
+            default:
+                fprintf(stderr, "予期しない文字です．");
+                return 1;
+        }
+    }
+
     printf("    ret\n");
 }
