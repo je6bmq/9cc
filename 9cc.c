e32d7c22c94d30f25a5a5aa779004e6aef7729e0
matchaBread <je6bmq@gmail.com>
2019-08-10 14:58:21 +0900

    impl step3

--- 9cc.c
+++ 9cc.c
@@ -1,5 +1,137 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdarg.h>
+
+typedef enum
+{
+    TK_RESERVED, // reserved symbol
+    TK_NUM,      // integer token
+    TK_EOF,      // EOF(End Of File) Token
+} TokenKind;
+
+typedef struct Token Token;
+
+struct Token
+{
+    TokenKind kind; // kind(type) of token
+    Token *next;    // next token pointer
+    int val;        // evaluated value if kind == TK_NUM
+    char *str;      // token string
+};
+
+Token *token; // current token
+
+/*
+    error function like fprintf
+ */
+void error(char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt); // obtain varargs after "fmt"
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+/*
+  return whether next token is reserved symbol or not.
+ */
+bool consume(char op)
+{
+    if (token->kind != TK_RESERVED || token->str[0] != op)
+    {
+        return false;
+    }
+    token = token->next;
+    return true;
+}
+
+/*
+    if next token is not reserved, abnormal exit.
+ */
+void expect(char op)
+{
+    if (token->kind != TK_RESERVED || token->str[0] != op)
+    {
+        error("'%c'ではありません", op);
+    }
+    token = token->next;
+}
+
+/*
+    if next token is not a number, abnormal exit.
+ */
+int expect_number()
+{
+    if (token->kind != TK_NUM)
+    {
+        error("数ではありません");
+    }
+    int val = token->val;
+    token = token->next;
+    return val;
+}
+
+/*
+    return whether current token is EOF or not.
+ */
+bool at_eof()
+{
+    return token->kind == TK_EOF;
+}
+
+/*
+    create new token and chain current token
+ */
+Token *new_token(TokenKind kind, Token *cur, char *str)
+{
+    Token *tok = calloc(1, sizeof(Token));
+    tok->kind = kind;
+    tok->str = str;
+    cur->next = tok;
+    return tok;
+}
+
+/*
+    tokenize from provided string
+ */
+Token *tokenize(char *p)
+{
+    Token head;
+    head.next = (NULL);
+    Token *cur = &head;
+
+    while (*p)
+    {
+        if (isspace(*p))
+        {
+            // skip space
+            p++;
+            continue;
+        }
+
+        if (*p == '+' || *p == '-')
+        {
+            cur = new_token(TK_RESERVED, cur, p++);
+            continue;
+        }
+
+        if (isdigit(*p))
+        {
+            cur = new_token(TK_NUM, cur, p);
+            cur->val = strtol(p, &p, 10);
+            continue;
+        }
+
+        error("トークナイズできません");
+    }
+
+    new_token(TK_EOF, cur, p);
+    return head.next;
+}
 
 int main(int argc, char **argv)
 {
@@ -9,28 +141,25 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    char* p = argv[1];
+    token = tokenize(argv[1]);
 
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    printf("    mov rax, %ld\n", strtol(p, &p, 10));
+    printf("    mov rax, %d\n", expect_number());
 
-    while(*p) {
-        switch(*p) {
-            case '+':
-                p++;
-                printf("    add rax, %ld\n",strtol(p, &p, 10));
-                break;
-            case '-':
-                p++;
-                printf("    sub rax, %ld\n",strtol(p, &p, 10));
-                break;
-            default:
-                fprintf(stderr, "予期しない文字です．");
-                return 1;
+    while (!at_eof())
+    {
+        if (consume('+'))
+        {
+            printf("    add rax, %d\n", expect_number());
+            continue;
         }
+
+        expect('-');
+        printf("    sub rax, %d\n", expect_number());
     }
 
     printf("    ret\n");
+    return 0;
 }
