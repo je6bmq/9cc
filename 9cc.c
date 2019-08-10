3167c4965f70f503d0d04428726919cbd04fd6d6
matchaBread <je6bmq@gmail.com>
2019-08-10 15:05:08 +0900

    impl step4

--- 9cc.c
+++ 9cc.c
@@ -23,6 +23,20 @@ struct Token
 };
 
 Token *token; // current token
+char *user_input;
+
+void error_at(char *loc, char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, "");
+    fprintf(stderr, "^ ");
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
 
 /*
     error function like fprintf
@@ -56,7 +70,7 @@ void expect(char op)
 {
     if (token->kind != TK_RESERVED || token->str[0] != op)
     {
-        error("'%c'ではありません", op);
+        error_at(token->str, "'%c'ではありません", op);
     }
     token = token->next;
 }
@@ -68,7 +82,7 @@ int expect_number()
 {
     if (token->kind != TK_NUM)
     {
-        error("数ではありません");
+        error_at(token->str, "数ではありません");
     }
     int val = token->val;
     token = token->next;
@@ -126,7 +140,7 @@ Token *tokenize(char *p)
             continue;
         }
 
-        error("トークナイズできません");
+        error_at(token->str, "トークナイズできません");
     }
 
     new_token(TK_EOF, cur, p);
@@ -140,6 +154,8 @@ int main(int argc, char **argv)
         fprintf(stderr, "引数の個数が正しくありません．");
         return 1;
     }
+
+    user_input = argv[1];
 
     token = tokenize(argv[1]);
 
