cc87bf49771745581578fde1e8140a586509148c
matchaBread <je6bmq@gmail.com>
2019-08-10 16:56:34 +0900

    impl step6

--- 9cc.c
+++ 9cc.c
@@ -43,6 +43,7 @@ struct Node
 
 Node *expr();
 Node *mul();
+Node *unary();
 Node *term();
 
 /*
@@ -221,17 +222,30 @@ Node *mul()
     {
         if (consume('*'))
         {
-            node = new_node(ND_MUL, node, term());
+            node = new_node(ND_MUL, node, unary());
         }
         else if (consume('/'))
         {
-            node = new_node(ND_DIV, node, term());
+            node = new_node(ND_DIV, node, unary());
         }
         else
         {
             return node;
         }
     }
+}
+
+Node *unary()
+{
+    if (consume('+'))
+    {
+        return term();
+    }
+    if (consume('-'))
+    {
+        return new_node(ND_SUB, new_node_num(0), term());
+    }
+    return term();
 }
 
 Node *term()
