b7c586ec99d74b1d2db2b38187dd45666952ceca
matchaBread <je6bmq@gmail.com>
2019-09-17 22:29:00 +0900

    add test case

---
+++ test/63.c
@@ -0,0 +1,16 @@
+int a;
+int b[20];
+int main()
+{
+    b[0] = 1;
+    b[1] = b[0];
+    a = 8;
+
+    if (a + b[0] + b[1] != 10)
+    {
+        printf("a + b[0] + b[1]!= 10\n");
+        exit(1);
+    }
+
+    return 0;
+}
---
+++ test/64.c
@@ -0,0 +1,15 @@
+int main()
+{
+    char a[2];
+    a[0] = 1;
+    a[1] = 2;
+
+    if (a[0] + a[1] == 3)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
