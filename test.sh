0cbc8efcc9431f5aa95f84b3bba5de7e542c9244
matchaBread <je6bmq@gmail.com>
2019-09-20 00:20:34 +0900

    create log file in test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,28 @@
 #!/bin/bash -xe
 cd test/
 
+TEST_LOG="../test.log"
+rm -f $TEST_LOG
+touch $TEST_LOG
+
 for file in $(\ls *.c)
 do
-    ../9cc $file > ${file%%.c}.s 2>/dev/null && gcc ${file%%.c}.s -o ${file%%.c}.o -no-pie && ./${file%%.c}.o
+    echo "file: $file" >>  $TEST_LOG
+    
+    set +x 
+    tmp=$(mktemp ./XXXXXX.tmp)
+    
+    set -x 
+    ../9cc $file > ${file%%.c}.s 2>$tmp && cat $tmp >> $TEST_LOG  
+    set +x
+
+    rm -f $tmp
+    
+    set -x  
+    gcc ${file%%.c}.s -o ${file%%.c}.o -no-pie >> $TEST_LOG 2>&1 && ./${file%%.c}.o >> $TEST_LOG 2>&1
+    set +x
+    
+    echo "" >>  $TEST_LOG
 done
+
 echo "OK"
